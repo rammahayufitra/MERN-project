e33f2019fa5b959f072bdbd765b79be94787cebd
ramma.fitra <rammafitracyt@gmail.com>
2023-04-24 14:16:11 +0700

    FRONT END inprogress UserList.js Done AddUser.js inPrpgress

---
+++ FULLSTACK1/frontend/src/App.js
@@ -0,0 +1,11 @@
+import UserList from "./components/UserList";
+
+function App() {
+  return (
+    <div className="container">
+        <UserList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ FULLSTACK1/frontend/src/components/UserList.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+// rafce
+const UserList = () => {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const response = await axios.get("http://localhost:8000/users");
+    setUser(response.data);
+  };
+
+  return (
+    <div className="columns">
+      <div className="column is-half">
+        <table className="table is-striped is-fullwidth mt-5">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.gender}</td>
+                <td>
+                    <button className="button is-info is-small">Edit</button>
+                    <button className="button is-danger is-small">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default UserList;
